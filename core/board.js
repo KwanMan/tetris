4c71853bb3f3766b2d7cbd37d7da81d99bd2f9d2
Tommy Kwan <tkwan196@gmail.com>
2017-05-06 13:30:03 +0100

    Cleanup

--- core/board.js
+++ core/board.js
@@ -2,40 +2,61 @@ import { BOARD_WIDTH, BOARD_HEIGHT } from './constants'
 
 export default function board (grid = {}) {
   return {
-    get (x, y) {
-      return grid[id([x, y])]
-    },
-    add (tetrix) {
-      return board(Object.assign({}, grid, toGrid(tetrix)))
-    },
-    clashesWith (tetrix) {
-      return tetrix.getCoords().some((coords) => grid[id(coords)])
-    },
-    removeCompleted () {
-      let newGrid = Object.assign({}, grid)
-      forEach(4, count => {
-        forEach(BOARD_HEIGHT, y => {
-          const full = every(BOARD_WIDTH, x => newGrid[id([x, y])])
-          if (full) {
-            newGrid = deleteRow(newGrid, y)
-            return false
-          }
-        })
-      })
-      return board(newGrid)
-    },
-    getVisual () {
-      const output = []
+    get,
+    add,
+    clashesWith,
+    removeCompleted,
+    getVisual,
+    deleteRow
+  }
+
+  function get (x, y) {
+    return grid[id([x, y])]
+  }
+
+  function add (tetrix) {
+    return board(Object.assign({}, grid, toGrid(tetrix)))
+  }
+
+  function clashesWith (tetrix) {
+    return tetrix.getCoords().some((coords) => grid[id(coords)])
+  }
+
+  function removeCompleted () {
+    let newBoard = board(Object.assign({}, grid))
+    forEach(4, count => {
       forEach(BOARD_HEIGHT, y => {
-        const row = []
-        forEach(BOARD_WIDTH, x => {
-          const trueY = BOARD_HEIGHT - y - 1
-          row.push(grid[id([x, trueY])])
-        })
-        output.push(row)
+        const full = every(BOARD_WIDTH, x => newBoard.get(x, y))
+        if (full) {
+          newBoard = newBoard.deleteRow(y)
+          return false
+        }
       })
-      return output
-    }
+    })
+    return newBoard
+  }
+
+  function getVisual () {
+    const output = []
+    forEach(BOARD_HEIGHT, y => {
+      const row = []
+      forEach(BOARD_WIDTH, x => {
+        const trueY = BOARD_HEIGHT - y - 1
+        row.push(get(x, trueY))
+      })
+      output.push(row)
+    })
+    return output
+  }
+
+  function deleteRow (yTarget) {
+    return board(Object.keys(grid).reduce((memo, key) => {
+      const [x, y] = key.split(',').map(i => Number(i))
+      if (y === yTarget) return memo
+      if (y < yTarget) memo[key] = grid[key]
+      if (y > yTarget) memo[id([x, y - 1])] = grid[key]
+      return memo
+    }, {}))
   }
 }
 
@@ -50,16 +71,6 @@ function every (limit, fn) {
     if (!fn(i)) return false
   }
   return true
-}
-
-function deleteRow (grid, yTarget) {
-  return Object.keys(grid).reduce((memo, key) => {
-    const [x, y] = key.split(',').map(i => Number(i))
-    if (y === yTarget) return memo
-    if (y < yTarget) memo[key] = grid[key]
-    if (y > yTarget) memo[id([x, y - 1])] = grid[key]
-    return memo
-  }, {})
 }
 
 function toGrid (tetrix) {
